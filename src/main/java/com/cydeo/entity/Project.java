feb3253e0d853d6add6753461877be5d2542f224
orazturkmen <hudayberdiyevoraz@gmail.com>
2024-06-04 12:59:46 +0200

    Project Controller

---
+++ src/main/java/com/cydeo/entity/Project.java
@@ -0,0 +1,36 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "projects")
+public class Project extends BaseEntity{
+    private String projectCode;
+    private String projectName;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate startDate;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate endDate;
+
+    @Enumerated(EnumType.STRING)
+    private Status projectStatus;
+
+    private String projectDetail;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "manager_id")
+    private User assignedManager;
+}
