9e0f3010d5aa24e46e5f09fc929521be136aa6aa
orazturkmen <hudayberdiyevoraz@gmail.com>
2024-06-04 21:11:13 +0200

    Task Entity

---
+++ src/main/java/com/cydeo/entity/Task.java
@@ -0,0 +1,37 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Status;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tasks")
+@Where(clause = "is_deleted=false")
+public class Task extends BaseEntity{
+
+
+    private String taskSubject;
+    private String taskDetail;
+
+    @Enumerated(EnumType.STRING)
+    private Status taskStatus;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate assignedDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id")
+    private User assignedEmployee;
+}
